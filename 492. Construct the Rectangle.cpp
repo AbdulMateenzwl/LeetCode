54ee9e69cd5bb990f1fac4d68676a2da9e014004
notrealmateen <abdulmateenkhanzwl@gmail.com>
2022-12-31 21:45:04 +0500

    added tasks

--- 492. Construct the Rectangle.cpp
+++ 492. Construct the Rectangle.cpp
@@ -1,11 +1,15 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> constructRectangle(int area) {
-        for(int i=sqrt(area);i>0;i--){
-            if(area%i==0){
-                return vector<int>{area/i,i};
+    vector<int> constructRectangle(int area)
+    {
+        for (int i = sqrt(area); i > 0; i--)
+        {
+            if (area % i == 0)
+            {
+                return vector<int>{area / i, i};
             }
         }
-         return vector<int>{area,1};
+        return vector<int>{area, 1};
     }
 };
