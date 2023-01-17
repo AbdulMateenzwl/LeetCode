6718067cc74f5d55c8b881cb7b43dbd55ed660c7
notrealmateen <abdulmateenkhanzwl@gmail.com>
2023-01-17 22:15:54 +0500

    subtract product and sum of int

--- 1281.subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1281.subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -1,12 +1,15 @@
-class Solution {
+class Solution
+{
 public:
-    int subtractProductAndSum(int n) {
-        int p=0,m=1;
-        while(n>0){
-            p+=n%10;
-            m*=n%10;
-            n/=10;
+    int subtractProductAndSum(int n)
+    {
+        int p = 0, m = 1;
+        while (n > 0)
+        {
+            m *= (n % 10);
+            p += (n % 10);
+            n /= 10;
         }
-        return m-p;
+        return m - p;
     }
 };
