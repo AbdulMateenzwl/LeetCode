945cb4c536b48faeb7683ed6cb63f5b615af48e5
notrealmateen <abdulmateenkhanzwl@gmail.com>
2023-01-17 22:25:55 +0500

    largest triangle area

---
+++ 1779.find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1779 lang=cpp
+ *
+ * [1779] Find Nearest Point That Has the Same X or Y Coordinate
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+        
+    }
+};
+// @lc code=end
+
