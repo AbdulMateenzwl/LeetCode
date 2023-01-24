b6e302108947d19b6c780793281f8bf8238eb1aa
notrealmateen <abdulmateenkhanzwl@gmail.com>
2023-01-24 23:50:01 +0500

    depth of binary tree

---
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        queue<TreeNode *> queueNode;
+        queueNode.push(root);
+        int depth=0;
+        while (!queueNode.empty())
+        {
+            int n = queueNode.size();
+            for (int i = 0; i < n; i++)
+            {
+                TreeNode *current = queueNode.front();
+                queueNode.pop();
+                if (current->left != NULL)
+                    queueNode.push(current->left);
+                if (current->right != NULL)
+                    queueNode.push(current->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
