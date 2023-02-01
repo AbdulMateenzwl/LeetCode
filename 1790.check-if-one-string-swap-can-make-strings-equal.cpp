aad20f470201aa8fe2b2b057d7dc856496a59e7c
notrealmateen <abdulmateenkhanzwl@gmail.com>
2023-02-01 23:23:12 +0500

    check if string is equal after one swap

--- 1790.check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790.check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,48 +1,30 @@
-/*
- * @lc app=leetcode id=1790 lang=cpp
- *
- * [1790] Check if One String Swap Can Make Strings Equal
- */
-
-// @lc code=start
 class Solution
 {
 public:
     bool areAlmostEqual(string s1, string s2)
     {
         if (s1 == s2)
-        {
             return true;
-        }
         int left = 0;
-        int right = s2.length()-1;
-        bool flag=true;
+        int right = s2.length() - 1;
+        bool flag = true;
         while (left < right)
         {
-            flag=true;
+            flag = true;
             if (s1[left] == s2[left])
             {
                 left++;
-                flag=false;
+                flag = false;
             }
             if (s1[right] == s2[right])
             {
                 right--;
-                flag=false;
+                flag = false;
             }
-            if(flag)
-            {
+            if (flag)
                 break;
-            }
         }
-        char ch = s2[left];
-        s2[left] = s2[right];
-        s2[right] = ch;
-        if (s1 == s2)
-        {
-            return true;
-        }
-        return false;
+        swap(s2[left], s2[right]);
+        return (s1 == s2);
     }
 };
-// @lc code=end
