6ebce279261e996c4c59d501d91fb9d7bb8cb206
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2024-05-30 07:21:43 +0000

    Refactor method 3 to use unordered_map for improved performance

--- 217.contains-duplicate.cpp
+++ 217.contains-duplicate.cpp
@@ -13,12 +13,24 @@ public:
         // return nums.size() > set<int>(nums.begin(), nums.end()).size(); 
 
         ///////////////////    Method 3
-        map<int, bool> myMap;
-        for (auto& num: nums) {
-            if (myMap.find(num) != myMap.end())
+        // map<int, bool> myMap;
+        // for (auto& num: nums) {
+        //     if (myMap.find(num) != myMap.end())
+        //         return true;
+        //     else
+        //         myMap[num] = true;
+        // }
+        // return false;
+
+        //////////////////////// MEthod 4
+        unordered_map<int,bool> map;
+        for(int num:nums){
+            if(map.find(num)==map.end()){
+                map[num]=true;
+            }
+            else {
                 return true;
-            else
-                myMap[num] = true;
+            }
         }
         return false;
     }
