7fadc66d9fcb709bc9bd6eabe0f69275462ce66f
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2024-06-13 09:14:48 +0000

    chore: Refactor threeSum function for improved performance and readability

---
+++ 15.3-sum.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
+            start , end = i+1, len(nums) - 1
+            while start < end:
+                # if nums[start] == nums[main]:
+                #     start +=1
+                #     continue
+                sumVal = nums[start] + nums[end] + a
+                if  sumVal == 0:
+                    result.append([a,nums[start],nums[end]])
+                    start  += 1
+                    while nums[start] == nums[start - 1] and start < end:
+                        start += 1
+                elif sumVal > 0:
+                    end -= 1
+                else:
+                    start += 1
+                    
+        return result
+# @lc code=end
+
