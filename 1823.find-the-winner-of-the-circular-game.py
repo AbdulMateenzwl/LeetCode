a3f54e9b00de9f93b9739506216d35285c11419e
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2024-07-08 17:57:32 +0000

    feat: Implement solution for finding the winner of the circular game

---
+++ 1823.find-the-winner-of-the-circular-game.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=1823 lang=python3
+#
+# [1823] Find the Winner of the Circular Game
+#
+
+# @lc code=start
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        circle: list[int] = [num for num in range(1, n + 1, 1)]
+        cur_ind: int = 0
+
+        while len(circle) != 1:
+            next_to_remove: int = (cur_ind + k - 1) % len(circle)
+            circle.pop(next_to_remove)
+            cur_ind = next_to_remove
+
+        return circle[0]
+# @lc code=end
+
