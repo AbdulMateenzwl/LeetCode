db1e930a9156c1069a193a873ffce7ce5785ea24
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2024-07-22 08:29:15 +0100

    feat: Refactor generateParenthesis function for improved readability and efficiency with sub function

--- 22.generate-parentheses.py
+++ 22.generate-parentheses.py
@@ -1,25 +1,16 @@
-#
-# @lc app=leetcode id=22 lang=python3
-#
-# [22] Generate Parentheses
-#
-
-# @lc code=start
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        result = []
-        self.dfs(n, n, "", result)
-        return result
+        res = []
+        def helper(l, r , strr):
+            if l == n and r == n:
+                res.append(strr)
+                return
 
-    def dfs(self, left: int, right: int, s: str, result: List[str]) -> None:
-        if left == 0 and right == 0:
-            result.append(s)
-            return
-
-        if left > 0:
-            self.dfs(left - 1, right, s + "(", result)
-        if right > left:
-            self.dfs(left, right - 1, s + ")", result)
-
-# @lc code=end
-
+            if l < n:
+                helper(l+ 1,r,strr+'(')
+            
+            if r < l:
+                helper(l, r + 1, strr+')')
+        
+        helper(0, 0, '')
+        return res
