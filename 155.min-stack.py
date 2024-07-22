39c863670cb6bf9908f2092eca222ec79ab34b32
AbdulMateenzwl <abdulmateenzwl@gmail.com>
2024-07-22 08:06:30 +0100

    chore: Refactor MinStack class for improved efficiency

--- 155.min-stack.py
+++ 155.min-stack.py
@@ -1,21 +1,13 @@
-#
-# @lc app=leetcode id=155 lang=python3
-#
-# [155] Min Stack
-#
-
-# @lc code=start
 class MinStack:
+
     def __init__(self):
         self.stk = deque()    
 
     def push(self, val: int) -> None:
-        minSoFar = 0
-        if (len(self.stk) == 0 or self.stk[-1][1] > val):
-            minSoFar = val 
+        if not self.stk:
+            self.stk.append([val,val])
         else:
-            minSoFar = self.stk[-1][1]
-        self.stk.append((val,minSoFar))
+            self.stk.append([val, min(self.stk[-1][1], val)])
 
     def pop(self) -> None:
         self.stk.pop()
@@ -25,22 +17,3 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.stk[-1][1]
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
-# @lc code=end
-
