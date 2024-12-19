967b83df344dfd39ddc14dd6674587ed5d22a6c9
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2024-12-19 16:44:41 +0000

    refactor: Rename stack variable for clarity in MinStack implementation

--- 155.min-stack.py
+++ 155.min-stack.py
@@ -1,19 +1,19 @@
 class MinStack:
 
     def __init__(self):
-        self.stk = deque()    
+        self.stack = deque()
 
     def push(self, val: int) -> None:
-        if not self.stk:
-            self.stk.append([val,val])
+        if not self.stack:
+            self.stack.append((val,val))
         else:
-            self.stk.append([val, min(self.stk[-1][1], val)])
+            self.stack.append((val,min(self.getMin(),val)))
 
     def pop(self) -> None:
-        self.stk.pop()
+        return self.stack.pop()[0]
 
     def top(self) -> int:
-        return self.stk[-1][0]
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.stk[-1][1]
+        return self.stack[-1][1]
