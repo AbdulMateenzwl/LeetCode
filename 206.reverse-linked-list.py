bc959bee6dcb05fd4a93552d7ed83218afda7e2a
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2025-01-05 10:59:29 +0000

    feat: Add solution for searching a 2D matrix feat: Simplify reverse linked list implementation

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -1,40 +1,11 @@
-#
-# @lc app=leetcode id=206 lang=python3
-#
-# [206] Reverse Linked List
-#
-
-# @lc code=start
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Solution 1
         pre = None
 
         while head:
-            next = head.next
-            head.next = pre
-            pre = head
-            head = next
-
-        return pre
-        
-        # Solution 2
-        stk = deque()
-
-        while head:
-            stk.append(head.val)
+            curr = head
             head = head.next
-
-        ans = tmp = ListNode()
-        while stk:
-            tmp.next = ListNode(val = stk.pop())
-            tmp = tmp.next
-        print(stk)
-        return ans.next
-# @lc code=end
-
+            curr.next = pre
+            pre = curr
+        
+        return pre
--- 74.search-a-2-d-matrix.py
+++ 74.search-a-2-d-matrix.py
@@ -23,5 +23,38 @@ class Solution:
 
         return False
         
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        top = 0
+        bottom = len(matrix) - 1
+
+        while top <= bottom:
+            mid = (top + bottom ) // 2
+            if target > matrix[mid][-1] :
+                top = mid + 1
+            elif target < matrix[mid][0]:
+                bottom = mid - 1
+            else:
+                break
+
+        if not (top <= bottom):
+            return False
+        
+        left = 0
+        right = len(matrix[0]) - 1
+        row = (top + bottom) // 2
+         
+        while left <= right:
+            mid = (left + right ) // 2
+
+            if target == matrix[row][mid]:
+                return True
+            elif target > matrix[row][mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False
+            
 # @lc code=end
 
